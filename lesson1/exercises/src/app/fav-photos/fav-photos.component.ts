efe611afe080e25ae5035b2fe9277588e07be8c0
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-04-11 14:49:59 -0400

    created ng project

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://i1.sndcdn.com/artworks-000080583661-0866k3-t500x500.jpg';
+  image2 = 'https://dafb3cv85j5xj.cloudfront.net/blog/wp-content/uploads/2016/07/bananya_feat.jpg';
+  image3 = 'https://m.media-amazon.com/images/M/MV5BYjlhNTA3Y2ItYjhiYi00NTBiLTg5MDMtZDJjMDZjNzVjNjJmXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg';
 
   constructor() { }
 
