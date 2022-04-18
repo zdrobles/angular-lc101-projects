9c810c4089b7d454e6804b512e20f31fcf7dadcf
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-04-18 01:19:19 -0400

    Added gutterCheck funtion to display orange if spaceship is out of bounds

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -11,7 +11,16 @@ export class AppComponent {
   takeOffEnabled: boolean = false;
   color: string = 'green';
   height: number = 0;
+  width: number = 0;
   message: string = 'Space shuttle ready for takeoff!';
+
+  gutterCheck(width: number, height: number) {
+    if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+  }
 
   handleTakeOff(rocketImage) {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
@@ -39,6 +48,7 @@ export class AppComponent {
       rocketImage.style.left = '0px';
       this.color = 'red';
       this.height = 0;
+      this.width = 0;
       this.message = 'Mission aborted.';
       this.takeOffEnabled = false;
     }
@@ -48,11 +58,13 @@ export class AppComponent {
     if (direction === 'right') {
       let movement = parseInt(rocketImage.style.left) + 10 + 'px';
       rocketImage.style.left = movement;
+      this.width = this.width + 10000;
     }
 
     if (direction === 'left') {
       let movement = parseInt(rocketImage.style.left) - 10 + 'px';
       rocketImage.style.left = movement;
+      this.width = this.width - 10000;
     }
 
     if (direction === 'up') {
@@ -66,6 +78,7 @@ export class AppComponent {
       rocketImage.style.bottom = movement;
       this.height = this.height - 10000;
     }
+    this.gutterCheck(this.width, this.height);
   }
 
 }
